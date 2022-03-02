6c03fd86626dbfd575b04a72d5a318a97859ba52
d.borisov <dim.borisov937@gmail.com>
2022-03-02 16:06:47 +0300

    init

---
+++ src/main/java/ru/otus/array/Application.java
@@ -0,0 +1,19 @@
+package ru.otus.array;
+import ru.otus.array.util.ArrayUtil;
+
+public class Application {
+    public static void main(String[] args) {
+        int[] array = ArrayUtil.generateArray(10);
+
+        Arrays task = new Arrays();
+        task.reverseArray(array);
+        task.average(array);
+
+        String left = "";
+        String right = "";
+        Anagram anagram = new Anagram();
+        boolean result = anagram.check(left, right);
+
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/ru/otus/array/Arrays.java
@@ -0,0 +1,24 @@
+package ru.otus.array;
+import ru.otus.array.util.ArrayUtil;
+public class Arrays {
+
+    public int[] reverseArray(int[] array) {
+        ArrayUtil.printArray(array);
+        int[] reverse = new int[array.length];
+        // TODO реализуйте код для создания перевернутого массива
+        for(int i = array.length; i!=0 ; i--){
+            for(int j = 0; j<array.length; j++){
+
+            }
+        }
+        return new int[0];
+    }
+
+    public float average(int[] array) {
+        ArrayUtil.printArray(array);
+
+        // TODO реализуйте код для расчета среднего значения
+
+        return 0;
+    }
+}
