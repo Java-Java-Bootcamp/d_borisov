3e2dfe9cbc8966b3faaa715e186f9c040ee6cdfe
d.borisov <dim.borisov937@gmail.com>
2022-03-02 16:34:52 +0300

    реализовал два метода reverseArray и average

--- src/main/java/ru/otus/array/Application.java
+++ src/main/java/ru/otus/array/Application.java
@@ -6,14 +6,16 @@ public class Application {
         int[] array = ArrayUtil.generateArray(10);
 
         Arrays task = new Arrays();
-        task.reverseArray(array);
-        task.average(array);
-
+        ArrayUtil.printArray(task.reverseArray(array));
+        System.out.println(task.average(array));
+/*
         String left = "";
         String right = "";
         Anagram anagram = new Anagram();
         boolean result = anagram.check(left, right);
 
         System.out.println(result);
+
+ */
     }
 }
--- src/main/java/ru/otus/array/Arrays.java
+++ src/main/java/ru/otus/array/Arrays.java
@@ -1,24 +1,27 @@
 package ru.otus.array;
+
 import ru.otus.array.util.ArrayUtil;
+
 public class Arrays {
 
     public int[] reverseArray(int[] array) {
         ArrayUtil.printArray(array);
         int[] reverse = new int[array.length];
-        // TODO реализуйте код для создания перевернутого массива
-        for(int i = array.length; i!=0 ; i--){
-            for(int j = 0; j<array.length; j++){
-
-            }
+        int j = 0;
+        for (int i = array.length-1; i != -1; i--) {
+            reverse[j] = array[i];
+            j++;
         }
-        return new int[0];
+        return reverse;
     }
 
     public float average(int[] array) {
         ArrayUtil.printArray(array);
-
-        // TODO реализуйте код для расчета среднего значения
-
-        return 0;
+        float sum = 0f;
+        for (int i: array
+             ) {
+            sum+=i;
+        }
+        return sum/array.length;
     }
 }
