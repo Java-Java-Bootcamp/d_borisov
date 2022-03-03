9d96395e5f26d799e372da63bcf3ed78678fcc7a
d.borisov <dim.borisov937@gmail.com>
2022-03-03 14:04:48 +0300

    16-oop-session

---
+++ src/main/java/ru/playjim/Application.java
@@ -0,0 +1,7 @@
+package ru.playjim;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/ru/playjim/ApplicationTest.java
@@ -0,0 +1,12 @@
+package ru.playjim;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+    @Test
+    void test() {
+
+    }
+}
