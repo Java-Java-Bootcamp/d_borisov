d5eaa83009ee1238615268ff6e6b528e208dd58c
d.borisov <dim.borisov937@gmail.com>
2022-03-04 12:25:18 +0300

    новый файл:    .gitignore 	изменено:      src/ru/playjim/Main.java

--- calculator/src/ru/playjim/Main.java
+++ calculator/src/ru/playjim/Main.java
@@ -1,20 +1,64 @@
 package ru.playjim;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        if(scanner.next().equals("+")) {
-            System.out.println(sum(a,b));
+        String[] operations = new String[]{"+", "-", "*", "/"};
+        System.out.println("Введите первое слагаемое: ");
+        int a = input();
+        System.out.println("Введите второе слагаемое: ");
+        int b = input();
+        System.out.println("Выберите номер операции: ");
+        for (int i = 0; i < operations.length; i++) {
+            System.out.println((i + 1) + ". " + operations[i]);
         }
-        else System.out.println("Ошибка: операция не известна");
+        switch (scanner.next()) {
+            case "1":
+                plus(a, b);
+                break;
+            case "2":
+                minus(a, b);
+                break;
+            case "3":
+                multiply(a, b);
+                break;
+            case "4":
+                division(a, b);
+            default:
+                System.out.println("Ошибка: операция не известна");
+                break;
+        }
     }
-    public static int sum (int a, int b){
-        return a + b;
+
+    public static void plus(int a, int b) {
+        System.out.println(a + "+" + b + "=" + (a + b));
+    }
+
+    public static void minus(int a, int b) {
+        System.out.println(a + "-" + b + "=" + (a - b));
+    }
+
+    public static void division(int a, int b) {
+        System.out.println(a + "/" + b + "=" + (a / b));
+    }
+
+    static void multiply(int a, int b) {
+        System.out.println(a + "*" + b + "=" + (a * b));
+    }
+
+    public static int input() {
+        int x;
+        do {
+            try {
+                x = new Scanner(System.in).nextInt();
+                return x;
+            } catch (InputMismatchException e) {
+                System.out.println("Введите число!");
+            }
+        } while (true);
     }
 }
